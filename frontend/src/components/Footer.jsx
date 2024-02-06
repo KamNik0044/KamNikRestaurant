bc1fb9e238fa5866f1c00beab7abc4c0013fe8f4
KamNik0044 <105980197+KamNik0044@users.noreply.github.com>
2024-02-06 19:24:32 +0000

    updates

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="container">
+        <div className="banner">
+          <div className="left">KAMNIK</div>
+          <div className="right">
+            <p>Stockport, Manchester</p>
+            <p>Open: 05:00 PM - 12:00 AM</p>
+          </div>
+        </div>
+        <div className="banner">
+          <div className="left">
+            <p>Developed By KAMNIK</p>
+          </div>
+          <div className="right">
+            <p>All Rights Reserved By KAMNIK</p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
